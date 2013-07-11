aa26d7933f5a0c7228891a3a4f63d37009c70eec
ramdev6498 <ramdev6498@gmail.com>
2013-07-11 08:07:55 +0000

    updated with index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs=require('fs');
+var buf= fs.ReadFileSync('index.html','utf-8');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
