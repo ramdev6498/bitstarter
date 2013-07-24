ec9a71a4fceaa5111e0339f89bfca3d05d606d4c
ramdev6498 <ramdev6498@gmail.com>
2013-07-24 14:43:02 +0000

    updated with extlink fold

--- web.js
+++ web.js
@@ -1,14 +1,22 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer()
+.use(express.logger())
+.use(express.static(__dirname + '/bootstrap'))
+.use(express.static(__dirname + '/extlinks'));
 
 app.get('/', function(request, response) {
   var fs=require('fs');
 var buf= new Buffer( fs.readFileSync('index.html','utf-8'));
   response.send(buf.toString());
-});
 
-var port = process.env.PORT || 5000;
+});
+app.get('/extlinks', function(req, res) {
+ var fs=require('fs');
+var buffer = fs.readFileSync('contact.html', 'utf-8');
+  response.send(buffer);
+});
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
